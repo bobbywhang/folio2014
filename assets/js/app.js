8b3ea1266b33b6ed6c299366f86024ca4995e807
bobbywhang <robertwhang@gmail.com>
2014-12-05 10:56:58 -0800

    cleaned up js and css files

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,10 @@ $(function () {
 	// "use strict";
 
 	
-$(".navToggle").click (function(){
-  $(this).toggleClass("open");
-  $("nav").toggleClass("open");
-});
+	$(".navToggle").click (function(){
+	  $(this).toggleClass("open");
+	  $("nav").toggleClass("open");
+	});
 	
 	function getGridSize() {
 		return (Modernizr.mq('(max-width:490px)')) ? 1 : 
@@ -20,6 +20,25 @@ $(".navToggle").click (function(){
 	// $.backstretch([
 	// 	"assets/img/background/1.jpg"
 	// ], {duration: 3800, fade: 1500});
+	//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navToggle").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
 	
 	
 	/* ---------------------------------------------------------
@@ -105,35 +124,35 @@ $(".navToggle").click (function(){
 	 *	Scroll arrow
 	 */
 	
-	$("#scroll").click(function () {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 1200);
-				return false;
-			}
-		}
-	});
+	// $("#scroll").click(function () {
+	// 	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	// 		var target = $(this.hash);
+	// 		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	// 		if (target.length) {
+	// 			$('html,body').animate({
+	// 				scrollTop: target.offset().top
+	// 			}, 1200);
+	// 			return false;
+	// 		}
+	// 	}
+	// });
 	
 	
 	/* ---------------------------------------------------------
 	 *	Timeline
 	 */
 	
-	$("[data-toggle='collapse']").on("click", function(e){
-		e.preventDefault();
-		var id = $('.v-icon[data-target="' + $(this).attr("data-target") + '"]');
-		var icon = $(id).children("i");
-		if(/fa-minus/i.test($(icon).attr("class"))){
-				$(icon).removeClass("fa-minus").addClass("fa-plus");
-		}
-		else{
-			$(icon).removeClass("fa-plus").addClass("fa-minus");
-		}
-	});
+	// $("[data-toggle='collapse']").on("click", function(e){
+	// 	e.preventDefault();
+	// 	var id = $('.v-icon[data-target="' + $(this).attr("data-target") + '"]');
+	// 	var icon = $(id).children("i");
+	// 	if(/fa-minus/i.test($(icon).attr("class"))){
+	// 			$(icon).removeClass("fa-minus").addClass("fa-plus");
+	// 	}
+	// 	else{
+	// 		$(icon).removeClass("fa-plus").addClass("fa-minus");
+	// 	}
+	// });
 	
 	
 	/* ---------------------------------------------------------
@@ -271,10 +290,10 @@ $(".navToggle").click (function(){
 	 *	CounterUp
 	 */
 	
-	$('.counter').counterUp({
-		delay: 100,
-		time: 2000
-	});
+	// $('.counter').counterUp({
+	// 	delay: 100,
+	// 	time: 2000
+	// });
 	
 	
 	// /* ---------------------------------------------------------
